7fa39f23dc1ab49cf0943b5590c93373ba259448
backseatviolist <when.possible@gmail.com>
2014-06-26 22:21:28 -0700

    remove extraneous .connect()

--- server/app.js
+++ server/app.js
@@ -2,15 +2,12 @@ var express = require('express'),
 	SnapCloud = require('./cloud').SnapCloud,
 	hex_sha512 = require('./sha512');
 
-SnapCloud.connect(function () {
-	console.log('connected');
-	SnapCloud.login('', hex_sha512(''), function () {
-		console.log('logged in!');
-	}, function () {
-		console.log('login failed :(');
-		console.log(arguments);
-	});
+var username = 'XXXXXXXX',
+	password = 'XXXXXXXX';
+
+SnapCloud.login(username, hex_sha512(password), function () {
+	console.log('logged in');
 }, function () {
-	console.log('connection failed :(');
+	console.log('login failed :(');
 	console.log(arguments);
 });
