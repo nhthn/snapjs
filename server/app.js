f3c71fb2517d345dc905dbee71a063b8c142cae4
backseatviolist <when.possible@gmail.com>
2014-07-01 16:03:55 -0700

    dummy client with Angularjs

--- server/app.js
+++ server/app.js
@@ -12,11 +12,14 @@ app.configure(function () {
 });
 
 function processProjectList(projects) {
-	var attr_whitelist = ['ProjectName', 'Updated', 'Notes', 'Public', 'Thumbnail'];
-	return projects.map(function (before) {
-		var after = {};
-		attr_whitelist.forEach(function (attr) { after[attr] = before[attr]; });
-		return after;
+	return projects.map(function (p) {
+		return {
+			name: p.ProjectName,
+			date: p.Updated,
+			notes: p.Notes,
+			public: p.Public,
+			thumbnail: p.Thumbnail
+		}
 	});
 }
 
@@ -25,6 +28,7 @@ app.post('/login', function (req, res) {
 	cloud = new SnapCloud('https://snapcloud.miosoft.com/miocon/app/login?_app=SnapCloud');
 	cloud.login(req.body.username, hex_sha512(req.body.password), function () {
 		cloud.getProjectList(function (projects) {
+			console.log(processProjectList(projects));
 			res.send({ projects: processProjectList(projects) });
 		}, function () {
 			res.send({ error: Array.prototype.slice.call(arguments) });
